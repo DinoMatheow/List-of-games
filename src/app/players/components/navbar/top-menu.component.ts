0f82f2015eab4c01258c790442479fc180d35fc0
Matheow <matius152014@gmail.com>
2025-04-09 10:26:43 -0500

    languaje button

--- src/app/players/components/navbar/top-menu.component.ts
+++ src/app/players/components/navbar/top-menu.component.ts
@@ -1,11 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, LOCALE_ID, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { LocaleService, localIdioma } from '../../services/locale.service';
+import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-top-menu',
   standalone: true,
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, UpperCasePipe],
   templateUrl: './top-menu.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TopMenuComponent { }
+export class TopMenuComponent {
+  currentLocale = signal(inject(LOCALE_ID));
+  localeService = inject(LocaleService);
+  nameUpper = signal('GG GAME');
+
+
+
+
+
+
+  changeLocale(locale: localIdioma) {
+    this.localeService.changeLocale(locale);
+  }
+
+ }
