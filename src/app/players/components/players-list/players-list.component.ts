632b806e2502d6be28e27f271a1603ac12bce4da
Matheow <matius152014@gmail.com>
2025-04-11 19:02:42 -0500

    playlist update

--- src/app/players/components/players-list/players-list.component.ts
+++ src/app/players/components/players-list/players-list.component.ts
@@ -2,16 +2,40 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { IDNamePlayers } from '../../interfaces/idName-players.interface';
 import { InfoPlayers } from '../../interfaces/info-players.interface';
 import { ImgLevel } from '../../interfaces/img-level-player.interface';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'players-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './players-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlayersListComponent {
-
- infoPlayer = input<InfoPlayers[]>([]);
+  infoPlayer = input<InfoPlayers[]>([]);
   players = input<IDNamePlayers[]>([]);
   imgLevelPlayer = input<ImgLevel>();
+
+  getTierBadgeClass(): string {
+    // Convierte el valor de 'tier' a minúsculas
+    const tier = (this.infoPlayer()[0]?.tier ?? this.infoPlayer()[1]?.tier)?.toLowerCase();
+
+    console.log('Tier:', tier);  // Verifica el valor del tier
+
+    switch (tier) {
+      case 'iron': return 'badge badge-secondary';
+      case 'bronze': return 'badge badge-warning';
+      case 'silver': return 'badge badge-neutral';
+      case 'gold': return 'badge badge-accent';
+      case 'platinum': return 'badge badge-success';
+      case 'emerald': return 'badge badge-info';
+      case 'diamond': return 'badge badge-primary';
+      case 'master': return 'badge badge-error';
+      case 'grandmaster':
+      case 'challenger':
+        return 'badge badge-error text-white';
+      default: return 'badge badge-neutral';
+    }
+  }
+
 }
